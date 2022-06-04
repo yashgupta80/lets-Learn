1481a0bbf10a59d3d818f38781b4f7e6d6d6bc88
Yash Gupta <76033745+yashgupta80@users.noreply.github.com>
2022-06-04 09:24:31 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -2,6 +2,6 @@ public class multiply {
     public  static  void main(String[] args){
         int d = 9;
         int e = 8;
-        System.out.println("d*e");
+        System.out.println(d*e);
     }
 }
